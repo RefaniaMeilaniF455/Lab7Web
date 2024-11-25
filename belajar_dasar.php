eda70f84cafc216dce6a473604af178f8c336f2c
RefaniaMeilaniF455 <refaniameilanifirdos@gmail.com>
2024-11-25 10:39:09 +0700

    Add files via upload

---
+++ belajar_dasar.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP DASAR</title>
+</head>
+<body>
+    <h1>Belajar PHP Dasar</h1>
+    <?php
+        echo "Hello World"
+    ?>
+    <h2>Variable PHP</h2>
+    <?php
+    $nim  = "312310173";
+    $nama = 'Refania Meilani Firdos';
+    echo "NIM  : " . $nim . "<br>";
+    echo "Nama : $nama";
+    ?>
+</body>
+</html>
